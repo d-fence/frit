144a1a5335a4e5fb3ab1b24940cc6b73a35819f7
Christophe Monniez <dfence.242@gmail.com>
2010-10-18 14:39:13 +0200

    Adding support for undeleted PST/OST files too.

--- frit/fritcommands/getmails.py
+++ frit/fritcommands/getmails.py
@@ -12,29 +12,45 @@ import fritutils.fritdb as fritModel
 import fritutils.fritemails
 import fritutils.fritprobe
 
+def getOutlookMails(fs,extension):
+    for filepath in fs.ExtensionsOriginalFiles(extension):
+        exportPath = os.path.join('.frit/extractions/emails/outlook/', filepath)
+        pathToCreate = os.path.join('.frit/extractions/emails/outlook/', os.path.split(filepath)[0])
+        pstPath = os.path.join(fs.fsMountPoint, filepath)
+        if os.path.isdir(exportPath + '.export'):
+            fritutils.termout.printWarning('Extraction path "%s" already exists. Not exporting.' % (exportPath + '.export'))
+        else:
+            fs.mount('getmails', 'Extracting Outlook emails')
+            if fritutils.fritprobe.pffProbe(pstPath):
+                if not os.path.isdir(pathToCreate):
+                    os.makedirs(pathToCreate)
+                fritutils.fritemails.pffExport(pstPath,exportPath)
+            else:
+                fritutils.termout.printWarning('%s is not a PFF file.' % pstPath)
+            fs.umount('getmails')
+
+def getOutlookUndeleted(fs):
+    for filepath in fs.listUndeleted():
+        if os.path.splitext(filepath)[1] == u'.pst' or os.path.splitext(filepath)[1] == u'.ost' :
+            cleanedPath = filepath.replace('.frit/extractions/','')
+            exportPath = os.path.join('.frit/extractions/emails/outlook/' , cleanedPath)
+            pathToCreate = os.path.join('.frit/extractions/emails/outlook/', os.path.split(cleanedPath)[0])
+            if os.path.isdir(exportPath + '.export'):
+                fritutils.termout.printWarning('Extraction path "%s" already exists. Not exporting.' % (exportPath + '.export'))
+            else:
+                if fritutils.fritprobe.pffProbe(filepath):
+                    if not os.path.isdir(pathToCreate):
+                        os.makedirs(pathToCreate)
+                    fritutils.fritemails.pffExport(filepath,exportPath)
+            
 def factory(Evidences,args):
     for evi in Evidences:
         for fs in evi.fileSystems:
             # Searching for PST and OST files first
             # Working on normal files first
-            for filepath in fs.ExtensionsOriginalFiles(u'.pst'):
-                exportPath = os.path.join('.frit/extractions/emails/outlook/', filepath)
-                pathToCreate = os.path.join('.frit/extractions/emails/outlook/', os.path.split(filepath)[0])
-                pstPath = os.path.join(fs.fsMountPoint, filepath)
-                if os.path.isdir(exportPath + '.export'):
-                    fritutils.termout.printWarning('Extraction path already exists. Not exporting.')
-                else:
-                    fs.mount('getmails', 'Extracting Outlook emails')
-                    if fritutils.fritprobe.pffProbe(pstPath):
-                        os.makedirs(pathToCreate)
-                        fritutils.fritemails.pffExport(pstPath,exportPath)
-                    else:
-                        fritutils.termout.printWarning('%s is not a PFF file.' % pstPath)
-                    fs.umount('getmails')
-            
-            # Working on undeleted filesize
+            getOutlookMails(fs,u'.pst')
+            getOutlookMails(fs,u'.ost')
+          
+            # Working on undeleted files
             # It's probably quicker to walk undelete files than to query db
-            for filepath in fs.listUndeleted():
-                if os.path.splitext(filepath)[1] == u'.pst':
-                    print filepath
-    
+            getOutlookUndeleted(fs)
