d55710c1bdf3979769937289dfb220231d781dfa
Christophe Monniez <dfence.242@gmail.com>
2010-12-13 12:04:53 +0100

    Creating .frit/logs if it doesn't exists.

--- frit/fritutils/fritlog.py
+++ frit/fritutils/fritlog.py
@@ -3,6 +3,13 @@
 import logging
 import os
 
+if not os.path.exists('.frit/logs'):
+    try:
+        os.mkdir('.frit/logs')
+    except:
+        fritutils.termout.printWarning("Cannot create .frit/logs directory.")
+        sys.exit(1)
+        
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
                     datefmt = '%Y-%m-%d %H:%M:%S',
@@ -10,10 +17,13 @@ logging.basicConfig(level=logging.INFO,
                     filemode='w')
 
 loggers = {
+        'mainfritLog' : logging.getLogger('frit.mainfrit')        
         'getmailsLog' : logging.getLogger('frit.getmails'),
         'fritobjectsLog' : logging.getLogger('frit.fritobjects'),
         'hashesLog': logging.getLogger('frit.hashes'),
         }
+
+
 
 def setLevels(level):
     if level == 'DEBUG':
