b85e41b058c0c92d2688905aebc3fa7f4ca0bfcf
Christophe Monniez <dfence.242@gmail.com>
2010-12-13 09:20:35 +0100

    Adding logging support for getmails command.

--- frit/fritcommands/getmails.py
+++ frit/fritcommands/getmails.py
@@ -11,16 +11,21 @@ import fritutils.fritobjects
 import fritutils.fritdb as fritModel
 import fritutils.fritemails
 import fritutils.fritprobe
+import fritutils.fritlog
+
+logger = fritutils.fritlog.loggers['getmailsLog']
 
 def getExportPath(filepath,fs):
     exportPath = os.path.join('.frit/extractions/emails/outlook/', fs.evidenceConfigName, fs.configName, filepath)
     return exportPath
 
 def getOutlookMailsFromWalk(fs,extension):
+    logger.info('Starting to search for "%s" files from walk' % extension)
     fs.mount('getmails', 'Walking for Outlook emails')
     for filepath in fs.listFiles():
         ext = os.path.splitext(filepath)[1]
         if ext == extension:
+            logger.info('File "%s" found, starting extraction' % filepath)
             filepath = filepath.replace(fs.fsMountPoint,'')
             if filepath[0] == '/':
                 filepath = filepath[1:]
@@ -39,12 +44,15 @@ def getOutlookMailsFromWalk(fs,extension):
     fs.umount('getmails')
 
 def getOutlookMailsFromDb(fs,extension):
+    logger.info('Starting to search for "%s" files from database' % extension)
     for filepath in fs.ExtensionsOriginalFiles(extension):
+        logger.info('File "%s" found' % filepath)
         exportPath = getExportPath(filepath,fs)
         pathToCreate = os.path.split(exportPath)[0]
         pstPath = os.path.join(fs.fsMountPoint, filepath)
         if os.path.isdir(exportPath + '.export'):
             fritutils.termout.printWarning('Extraction path "%s" already exists. Not exporting.' % (exportPath + '.export'))
+            logger.info('Extraction path "%s" already exists. Not exporting.' % (exportPath + '.export'))
         else:
             fs.mount('getmails', 'Extracting Outlook emails')
             if fritutils.fritprobe.pffProbe(pstPath):
@@ -70,12 +78,17 @@ def getOutlookUndeletedFromDb(fs):
                     fritutils.fritemails.pffExport(filepath,exportPath)
             
 def factory(Evidences,args):
+    logger.info('Starting getmails command')
     if args and '--walk' in args:
+        logger.info('Using "--walk" argument.')
         for evi in Evidences:
+            logger.info('Working on evidence "%s"' % evi.configName)
             for fs in evi.fileSystems:
+                logger.info('Working on filesystem "%s"' % fs.configName)
                 getOutlookMailsFromWalk(fs,u'.pst')
                 getOutlookMailsFromWalk(fs,u'.ost')
     else:
+        logger.info('Searching mails from database.')
         if fritModel.dbExists():
             for evi in Evidences:
                 for fs in evi.fileSystems:
