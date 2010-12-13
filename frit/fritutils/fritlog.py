cce1b251fd8a797201a324b2aeef63f64448b583
Christophe Monniez <dfence.242@gmail.com>
2010-12-13 09:18:46 +0100

    Adding a logging module.

---
+++ frit/fritutils/fritlog.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import logging
+import os
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt = '%Y-%m-%d %H:%M:%S',
+                    filename='.frit/logs/frit-' +  str(os.getpid()) + '.log',
+                    filemode='w')
+
+loggers = { 'getmailsLog' : logging.getLogger('frit.getmails'), 'fritobjectsLog' : logging.getLogger('frit.fritobjects')}
+
+def setLevels(level):
+    if level == 'DEBUG':
+        l = logging.DEBUG
+    elif level == 'INFO':
+        l = logging.INFO
+    for logger in loggers:
+        loggers[logger].setLevel(l)
+
+
+
