bef86795612d3a00ba4bd15affcd9417a0e0a3fe
Christophe Monniez <christophe.monniez@fccu.be>
2015-07-03 09:38:08 +0200

    Fixing typo.

--- frit/fritcommands/carving.py
+++ frit/fritcommands/carving.py
@@ -16,6 +16,6 @@ logger = fritutils.fritlog.loggers['carvingLog']
 
 def factory(Evidences, args, options):
     if not options:
-        fritutils.termout.printWarning"You have to choose the tool to use:")
+        fritutils.termout.printWarning("You have to choose the tool to use:")
         fritutils.termout.printWarning("    --photorec")
 
