ca64a5caeb2cebedced396841b8dbc06cb231d41
Christophe Monniez <christophe.monniez@fccu.be>
2015-07-03 09:26:04 +0200

    Adding a basic skeleton for carving module.

---
+++ frit/fritcommands/carving.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+"""
+carving
+This command is used to make carving with various tools / methods
+An option specifying the tool to use is mandatory (I know, mandatory option is not an option)
+Tools :
+--photorec
+You can choose to carve the whole filesystems or the unallocated space only:
+--unalloc
+"""
+import os
+import fritutils
+import fritutils.fritlog
+
+logger = fritutils.fritlog.loggers['carvingLog']
+
+def factory(Evidences, args, options):
+    if not options:
+        fritutils.termout.printError("You have to choose the tool to use:")
+        fritutils.termout.printError("    --photorec")
+
